4b82548443c54428914156a8ad46e3268627fc8b
Namanyay Goel <mail@namanyayg.com>
2014-06-29 00:01:15 +0530

    Deploying

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-$(window).on("load",function(){$cont=$(".js-isotope"),$cont.isotope({itemSelector:".sympl",columnWidth:"25%",sortBy:"random"}),$cont.isotope("shuffle"),$(window).on("resize",function(){$cont.isotope()})});
+$(window).on("load",function(){$cont=$(".js-isotope"),$cont.isotope({itemSelector:".sympl",columnWidth:"25%",sortBy:"random",gutter:"0"}),$cont.isotope("shuffle"),$(window).on("resize",function(){$cont.isotope()})});
