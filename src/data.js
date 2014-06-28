182e7d562ae8e85f3d8d3e4dead0e22851bb4f4a
Namanyay Goel <mail@namanyayg.com>
2014-06-28 13:11:45 +0530

    Removing site and creating basic stuff

--- src/data.js
+++ src/data.js
@@ -1,1 +1,80 @@
-{}
+{
+  "fonts": [
+    {
+      "name": "Geo",
+      "from": "google",
+      "type": ["display", "sans-serif"]
+    },
+    {
+      "name": "Open Sans",
+      "from": "google",
+      "type": ["sans-serif"]
+    },
+    {
+      "name": "Lora",
+      "from": "google",
+      "type": ["serif"]
+    },
+    {
+      "name": "Lato",
+      "from": "google",
+      "type": ["sans-serif"]
+    },
+    {
+      "name": "Actor",
+      "from": "google",
+      "type": ["sans-serif"]
+    },
+    {
+      "name": "Maven Pro",
+      "from": "google",
+      "type": ["sans-serif"]
+    },
+    {
+      "name": "Montserrat",
+      "from": "google",
+      "type": ["sans-serif"]
+    },
+    {
+      "name": "Arvo",
+      "from": "google",
+      "type": ["serif", "slab serif"]
+    },
+    {
+      "name": "EB Garamond",
+      "from": "google",
+      "type": ["serif"]
+    },
+    {
+      "name": "Kameron",
+      "from": "google",
+      "type": ["serif", "slab serif"]
+    },
+    {
+      "name": "Varela Round",
+      "from": "google",
+      "type": ["sans-serif"]
+    },
+    {
+      "name": "Quicksand",
+      "from": "google",
+      "type": ["sans-serif"]
+    },
+    {
+      "name": "Dosis",
+      "from": "google",
+      "type": ["sans-serif"]
+    },
+    {
+      "name": "Josefin Sans",
+      "from": "google",
+      "type": ["sans-serif"]
+    },
+    {
+      "name": "Gudea",
+      "from": "google",
+      "type": ["sans-serif"]
+    }
+
+  ]
+}
