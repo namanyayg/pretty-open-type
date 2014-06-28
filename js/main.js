8f8dabf8a8783e72e670ba54c1dac38326c41100
Namanyay Goel <mail@namanyayg.com>
2014-06-28 19:06:35 +0530

    Basic stuff ready!

--- js/main.js
+++ js/main.js
@@ -1,14 +1,1 @@
-// include plugins/fit-columns.js
-
-;$(window).on('load', function() {
-  $cont = $('.js-isotope')
-  $cont.isotope({
-    itemSelector: '.sympl',
-    columnWidth: '25%'
-    // layoutMode: 'fitColumns'
-  });
-
-  $(window).on('resize', function() {
-    $cont.isotope();
-  });
-});
+$(window).on("load",function(){$cont=$(".js-isotope"),$cont.isotope({itemSelector:".sympl",columnWidth:"25%",sortBy:"random"}),$cont.isotope("shuffle"),$(window).on("resize",function(){$cont.isotope()})});
